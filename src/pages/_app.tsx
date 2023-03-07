fd6f9b4e28ae6182ac72bf11e7b472c4dc49e6ff
Bogdan_Todorow <andrewbagio@yahoo.com>
2023-03-07 16:56:20 +0000

    deleting unused imports

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
 import "@/styles/_app.css";
 
 export default function App() {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,3 @@
-import Head from 'next/head'
-import Image from "next/image";
 import App from "./_app";
 
 export default function Home() {
