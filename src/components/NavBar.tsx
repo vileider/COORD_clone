f2d60cb1d75990653fd2f43f46626caddbae7242
BowedYapper <bowedyapper@live.co.uk>
2023-03-22 16:21:59 +0000

    added icon to settings button

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,6 @@
 import Link from 'next/link';
 
 export default function NavBar() {
-
   return (
     <>
       <div>
@@ -9,8 +8,7 @@ export default function NavBar() {
           className="text-white py-1 px-4 rounded px-2 border border-white absolute left-0 top-0"
           href="/settings"
         >
-          {' '}
-          Settings{' '}
+          <span className="material-icons text-white">settings</span> Settings{' '}
         </Link>
 
         <Link
