4f6ebe9f409dc5c3e3beb0ada6d0c82a48cb6582
Lauren Keenan <19015831@uhi.ac.uk>
2023-03-24 12:05:20 +0000

    displays dummy data from inside the file

---
+++ src/reusableComponents/Display.tsx
@@ -0,0 +1,11 @@
+
+const Address : string = "642 address lane 000 000"
+
+export default function DisplayAddress() {
+
+    return(<>
+    <div className="text-white py-1 px-4 rounded px-2 border border-white box-content h-10 w-64 ...">
+    {Address}
+    </div>
+    </>)
+};
