6a2d6205aa67b830499408f29f0d0e5fa97e38c7
Lauren Keenan <19015831@uhi.ac.uk>
2023-03-25 15:33:51 +0000

    eslint changes

--- src/reusableComponents/Display.tsx
+++ src/reusableComponents/Display.tsx
@@ -2,10 +2,11 @@
 import { Address } from '@/components/NavBar';
 
 export default function DisplayAddress() {
-
-    return(<>
-    <div className="text-white py-1 px-4 rounded px-2 border border-white box-content h-10 w-64 ...">
-    {Address}
-    </div>
-    </>)
-};
+  return (
+    <>
+      <div className="text-white py-1 px-4 rounded px-2 border border-white box-content h-10 w-64 ...">
+        {Address}
+      </div>
+    </>
+  );
+}
