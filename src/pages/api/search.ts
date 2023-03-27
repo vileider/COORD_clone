bddb5e92b397e74aaf4509c7906e2b6665a6fcd0
Bogdan_Todorow <andrewbagio@yahoo.com>
2023-03-27 05:09:24 +0100

    changing the way JSON is tested. Reorganizing response conditions

--- src/pages/api/search.ts
+++ src/pages/api/search.ts
@@ -1,34 +1,38 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-const validatingJson = (str: string) => {
-  try {
-    JSON.parse(str);
-  } catch (e) {
-    return false;
+const validatingJson = (value: JSON) => {
+  if (value != null && typeof value != 'string') {
+    const valueS: string = JSON.stringify(value);
+    try {
+      JSON.parse(valueS);
+      return true;
+    } catch (e) {
+      return false;
+    }
   }
-  return true;
+  return false;
 };
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<string>
+  res: NextApiResponse<JSON>
 ) {
   //this will be deleted
   const delayResponse = () => {
-    return new Promise<string>((resolve) => {
+    return new Promise<JSON>((resolve) => {
       setTimeout(() => {
-        resolve('{"results":"available"}');
+        resolve([{ "exmpleJSON": 'true' }])
       }, 2000);
     });
   };
 
-  const results = await delayResponse();
-
-  if (validatingJson(results)) {
-    try {
-      res.status(200).json(results);
-    } catch (err) {
-      res.status(500).json('{ "results": "error" }');
-    }
-  } else res.status(200).json('{ results: "not available" }');
+  const resolve = await delayResponse();
+  try {
+    if (validatingJson(resolve)) {
+      res.status(200).json(resolve);
+    } else {
+      res.status(200).json([{ "results": "not available" }]);}
+  } catch (err) {
+    res.status(500).json([{ "results": err }]);
+  }
 }
