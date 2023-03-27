d1c65f485cbdd213d5904837c237367c0aeeba1d
Lauren Keenan <19015831@uhi.ac.uk>
2023-03-27 20:29:17 +0100

    centers display list and adds spacing

--- src/pages/results.tsx
+++ src/pages/results.tsx
@@ -4,14 +4,14 @@ import Address from '@/components/NavBar';
 export default function Results() {
   return (
     <>
-      <div className="container m-auto grid grid-cols-1 gap-4">
-        <div className="relative">
+      <div className="container m-auto grid grid-cols-1 gap-4 place-items-center">
+        <div className="">
           <div className="text-xl ...">Results</div>
           <div>
-            <div>
+            <div className="py-1">
               <DisplayAddress {...Address} />
             </div>
-            <div>
+            <div className="py-1">
               <DisplayAddress {...Address} />
             </div>
           </div>
--- src/reusableComponents/Display.tsx
+++ src/reusableComponents/Display.tsx
@@ -1,9 +1,10 @@
-export default function DisplayAddress(Address: string) {
+import { Address } from '@/components/NavBar';
+
+export default function DisplayAddress() {
   return (
     <>
-      {/* {Address} */}
       <div className="text-white py-1 px-4 rounded px-2 border border-white box-content h-10 w-64 ...">
-        {Address}
+        <h3>{Address}</h3>
       </div>
     </>
   );
