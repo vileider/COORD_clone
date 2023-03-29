cf8c7d753e04634ef7f0b185846356f9c5bdf3c4
Lauren Keenan <19015831@uhi.ac.uk>
2023-03-29 15:50:51 +0100

    imports searchbar, removes placeholder code

--- src/pages/Searchpage.tsx
+++ src/pages/Searchpage.tsx
@@ -1,13 +1,9 @@
+import { SearchBar } from '@/components/SearchBar';
+
 export default function SearchPage() {
-  const searchElements = (
-    <div className="search-elements">
-      <div className="localization-tracking">LoTr</div>
-      <div className="top-box">top box</div>
-      <div className="search-results"> search results</div>
-      <div className="bottom-box">bottom box</div>
-      <div className="submit-search"> button</div>
-      <i></i>
-    </div>
+  return (
+    <>
+      <SearchBar />
+    </>
   );
-  return <>{searchElements}</>;
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,14 @@
 import TopBar from '@/components/TopBar';
-import Input from '@/reusableComponents/input';
 import SearchPage from './Searchpage';
 
 export default function Home() {
   const mainContent = (
     <div className="main-page">
       <div className="top-bar">
-        <div className="left-side">
-          {/* <div className='user'>user</div>
-          <div className='config'>config</div> */}
-        </div>
-        {/* <div className='right-side'>Login</div> */}
+        <div className="left-side"></div>
       </div>
       <TopBar />
       <SearchPage />
-      <Input />
     </div>
   );
   return <>{mainContent}</>;
