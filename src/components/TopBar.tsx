67ca94cd00d2d7b9410a01f775ebc5d6a8511685
Timi Havran <21008549@uhi.ac.uk>
2023-03-30 09:12:30 +0100

    Fixed the position of logo

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -4,8 +4,11 @@ export default function TopBar() {
   return (
     <>
       {/*<NavBar />*/}
-      <div className="top-bar">
-        <img className="logo" src="https://cdn.coord.uk/coord.png" />
+      <div className="top-bar bg-[#dadada] h-16 drop-shadow-xl grid grid-rows-1 grid-cols-3 gap-4">
+        <img
+          className="logo h-24 col-start-2 col-span-2"
+          src="https://cdn.coord.uk/coord.png"
+        />
         {/*<div className="left-side">
           <div className="user">user</div>
           <div className="config">config</div>
