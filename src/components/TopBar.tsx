fe361e8ad52e22366ebc2ce7b0f79dcefc2b3086
Timi Havran <21008549@uhi.ac.uk>
2023-03-30 09:21:19 +0100

    Added colour for dark mode

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -4,7 +4,7 @@ export default function TopBar() {
   return (
     <>
       {/*<NavBar />*/}
-      <div className="top-bar bg-[#dadada] h-16 drop-shadow-xl grid grid-rows-1 grid-cols-3 gap-4">
+      <div className="top-bar bg-[#dadada] dark:bg-[#0C0C0C] h-16 drop-shadow-xl grid grid-rows-1 grid-cols-3 gap-4">
         <img
           className="logo h-24 col-start-2 col-span-2 absolute top-[-25%]"
           src="https://cdn.coord.uk/coord.png"
