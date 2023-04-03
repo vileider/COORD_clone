72462589a46046170ce7989bdf290e9bb4823acf
Lauren Keenan <19015831@uhi.ac.uk>
2023-04-03 22:10:57 +0100

    search button redirects to results page

--- src/components/SearchButton.tsx
+++ src/components/SearchButton.tsx
@@ -1,25 +1,12 @@
-import { useState } from 'react';
+import Router from 'next/router';
 import Input from '@/reusableComponents/input';
 
-interface InputProps {
-  input: string;
-  setInput: (input: string) => void;
-}
-
-export function SearchButton(props: InputProps) {
-  // const newAddress = props.input;
-  // const [Address, setAddress] = useState<string>('');
-  const { input, setInput } = props;
-  const handleClick = (input: string) => {
-    setInput(input);
-  };
-
+export function SearchButton() {
   return (
     <>
       <div className="">
-        {/* {Address} */}
         <Input />
-        <button onClick={() => handleClick}>
+        <button onClick={() => Router.push('/results')}>
           <span className="material-icons text-white">search</span>
         </button>
       </div>
