aac54a9841b8bab585b72aeb06ed01a49b87cd92
Timi Havran <21008549@uhi.ac.uk>
2023-04-03 11:41:26 +0100

    Added the grid layout to the input box

--- src/reusableComponents/input.tsx
+++ src/reusableComponents/input.tsx
@@ -7,7 +7,7 @@ const Input = () => {
   };
 
   return (
-    <>
+    <div className="input-container">
       <div>{input}</div> {/* For test to see the input field is working */}
       <input
         type="text"
@@ -16,7 +16,10 @@ const Input = () => {
         name="input"
         onChange={handleChange}
       />
-    </>
+      <span className="material-icons dark:stroke-[#e25812] w-6 h-6 relative top-[-200%] left-[55%] col-end-4">
+        search
+      </span>
+    </div>
   );
 };
 
