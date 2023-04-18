f89a71e2ae0b56d5fddc12c7556fafa004cb6517
BowedYapper <bowedyapper@live.co.uk>
2023-04-18 16:17:19 +0100

    adding test state change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,21 @@
 //import TopBar from '@/components/TopBar';
 import Input from '@/reusableComponents/input';
 import SearchPage from './Searchpage';
-
+import { GlobalContext } from '@/context/globalContext';
+import React from 'react';
 export default function Home() {
+  const { data, setData } = React.useContext(GlobalContext);
+  const { someOtherState } = data;
+  React.useEffect(() => {
+    setTimeout(() => {
+      setData({ someOtherState: 'It worked' });
+      console.log('Fired setData');
+    }, 1000);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   const mainContent = (
     <div className="main-page">
-      {/*<div className="top-bar">
-        <div className="left-side">
-           <div className='user'>user</div>
-          <div className='config'>config</div>
-        </div>
-         <div className='right-side'>Login</div> 
-      </div>
-      <TopBar />*/}
+      {someOtherState}
       <SearchPage />
       <Input />
     </div>
