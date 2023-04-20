d8a8648d6babd4f1160d6c6fd0216eb8dd2a8523
Bogdan_Todorow <andrewbagio@yahoo.com>
2023-04-20 16:10:49 +0100

    adding another object argument for JSON

--- src/context/globalContext.ts
+++ src/context/globalContext.ts
@@ -3,12 +3,14 @@ import React from 'react';
 export type GlobalContextType = {
   addressSearchString?: string;
   someOtherState?: string;
+  searchResults?: JSON;
 };
 
 export const contextDefaultValues = {
   data: {
     addressSearchString: '',
     someOtherState: 'This is a test to see if state is working',
+    searchResults: {},
   } as GlobalContextType,
   // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars, @typescript-eslint/no-empty-function
   setData: (state: GlobalContextType) => {},
